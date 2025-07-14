2b9a334a81b4e56f3a9e6564c7a4e68f4834231d
enjoy06 <152522310+enjoy06@users.noreply.github.com>
2025-07-14 20:35:26 +0700

    Update src.js

--- src.js
+++ src.js
@@ -1,3 +1,16 @@
-const looking = new URLSearchParams(window.location.search);
-const directed = looking.get('url');
-window.location.href = 'https://www.threads.com/';
+<script type="text/javascript">
+$.get('https://ipapi.co/country/', function(country){
+console.log(country)
+})
+
+if (country = "ID") {
+  window.location.replace("https://www.google.com");
+}
+else if (country = ["FR","IT","DE","CH"]) {
+  window.location.replace("EU.html");
+}
+else {
+  window.location.replace("US.html");
+}
+
+</script>
