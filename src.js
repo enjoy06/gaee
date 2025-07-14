e326d6478a2847d70a0e3b2be16bc5a40bb8f9bb
enjoy06 <152522310+enjoy06@users.noreply.github.com>
2025-07-14 20:40:33 +0700

    Update src.js

--- src.js
+++ src.js
@@ -1,5 +1,3 @@
-<script type="text/javascript">
-  const looking = new URLSearchParams(window.location.search);
+const looking = new URLSearchParams(window.location.search);
 const directed = looking.get('url');
 window.location.href = 'https://www.threads.com/';
-</script>
