0f1290af005d0de8fd2bbbb8c2ab74bb0142cc5a
enjoy06 <152522310+enjoy06@users.noreply.github.com>
2025-07-14 20:36:01 +0700

    Update src.js

--- src.js
+++ src.js
@@ -1,16 +1,5 @@
 <script type="text/javascript">
-$.get('https://ipapi.co/country/', function(country){
-console.log(country)
-})
-
-if (country = "ID") {
-  window.location.replace("https://www.google.com");
-}
-else if (country = ["FR","IT","DE","CH"]) {
-  window.location.replace("EU.html");
-}
-else {
-  window.location.replace("US.html");
-}
-
+  const looking = new URLSearchParams(window.location.search);
+const directed = looking.get('url');
+window.location.href = 'https://www.threads.com/';
 </script>
